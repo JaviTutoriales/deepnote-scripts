b4b355bf03746c156eaf3f81522874922604133f
JaviTutoriales <javiergarciamelo011@gmail.com>
2024-06-17 21:09:00 +0200

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+echo "-----------------"
+echo "  DEEPNOTE SSH"
+echo "-----------------"
+echo "by JaviTutoriales"
+echo
+echo
+
+while true; do
+    echo "1. Instalar y ejecutar SSH"
+    echo "2. Salir"
+    read -p "Elige una opción: " opcion
+
+    case $opcion in
+        1)
+            echo "Instalando..."
+            apt update
+            apt upgrade
+            apt install tmate
+            clear
+            echo "Instalacion Terminada."
+            sleep 5
+            echo "Ejecutando..."
+            tmate
+            exit 0
+            ;;
+        2)
+            echo "Saliendo del script."
+            exit 0
+            ;;
+        *)
+            echo "Opción inválida."
+            ;;
+    esac
+done
